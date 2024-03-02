65d4c95d7b2c585042f9ced71e0d02d8e17332b6
MichalWojcik0 <michal_wojcik@zohomail.eu>
2024-03-02 20:06:24 +0100

    various CCs, panning scales by zoom minor bugfixes to panning

--- test-clients/website/src/user-inputs.js
+++ test-clients/website/src/user-inputs.js
@@ -3,26 +3,20 @@ let moveOnce = [0.0, 0.0, 0.0, 0.0];
 let zoom = 0.0;
 let camZoom = -2.5;
 let zoomCoef = 1 / (camZoom + 3.5);
-const camZoomMin  = -2.5;
-const camZoomMax  = -0.5;
+const camZoomMin = -2.5;
+const camZoomMax = -0.5;
 
-let camPan  = [0.0, 0.0, 0.0];
+let camPan = [0.0, 0.0, 0.0];
 const camPanLimitVert = 1.0;
-const camPanLimitHor  = 1.6;
+const camPanLimitHor = 1.6;
 
-// camPan[1] -= getKeyPan(0) * (camPan[1] > -camPanLimitVert); 
-// camPan[1] += getKeyPan(2) * (camPan[1] < camPanLimitVert); 
-// camPan[0] -= getKeyPan(3) * (camPan[0] > -camPanLimitHor); 
-// camPan[0] += getKeyPan(1) * (camPan[0] < camPanLimitHor); 
-
-const speed = 0.05;
+const speed = 0.03;
 const mouseCoef = 0.003;
 const zoomFactor = 0.05;
 let isLeftDown = false;
 let isMiddleDown = false;
 let isRightDown = false;
 let mouseInCanvas = false;
-let prevMouseMove = null;
 
 /**
  * 
@@ -42,61 +36,60 @@ export function initWASD(canvas) {
     canvas.oncontextmenu = () => false;
     document.onmousedown = e => {
         mouseInCanvas = e.explicitOriginalTarget === canvas;
+        if(e.explicitOriginalTarget !== canvas) mov = [0.0, 0.0, 0.0, 0.0];
         if (e.button & 0) isLeftDown = true;
         if (e.button & 1) isMiddleDown = true;
         if (e.button & 2) isRightDown = true;
     }
     document.onmouseup = e => {
-        mov = [0.0, 0.0, 0.0, 0.0];
         isLeftDown *= 1 - (e.button == 0);
         isMiddleDown *= 1 - (e.button == 1);
         isRightDown *= 1 - (e.button == 2);
     }
     canvas.onmousemove = e => {
         if (isRightDown) {
-            moveOnce[0] = zoomCoef * mouseCoef * e.movementY;
-            moveOnce[1] = zoomCoef * mouseCoef * e.movementX;
+            moveOnce[1] = mouseCoef * e.movementY;
+            moveOnce[0] = mouseCoef * e.movementX;
         }
-        prevMouseMove = e;
     }
     document.onkeydown = e => {
-        if(mouseInCanvas) {
+        if (mouseInCanvas) {
             switch (e.key) {
                 case 'w': case 'W':
-                    mov[0] = speed;
+                    mov[1] += (1 - e.repeat) * speed;
                     break;
                 case 's': case 'S':
-                    mov[2] = speed;
+                    mov[1] -= (1 - e.repeat) * speed;
                     break;
                 case 'd': case 'D':
-                    mov[3] = speed;
+                    mov[0] -= (1 - e.repeat) * speed;
                     break;
                 case 'a': case 'A':
-                    mov[1] = speed;
+                    mov[0] += (1 - e.repeat) * speed;
                     break;
             }
         }
     }
     document.onkeyup = e => {
-        switch (e.key) {
-            case 'w': case 'W':
-                mov[0] = 0;
-                break;
-            case 's': case 'S':
-                mov[2] = 0;
-                break;
-            case 'd': case 'D':
-                mov[3] = 0;
-                break;
-            case 'a': case 'A':
-                mov[1] = 0;
-                break;
+        let coef = 1;
+        if (mouseInCanvas) {
+            switch (e.key) {
+                case 'w': case 'W':
+                    coef = -1;
+                case 's': case 'S':
+                    mov[1] += coef * speed;
+                    break;
+                case 'a': case 'A':
+                    coef = -1;
+                case 'd': case 'D':
+                    mov[0] += coef * speed;
+                    break;
+            }
         }
     }
     canvas.onwheel = e => {
         e.preventDefault();
-        const sign = Math.sign(e.deltaY);
-        const res =  sign * zoomFactor / Math.abs(camZoom)
+        const res = Math.sign(e.deltaY) * zoomFactor / Math.abs(camZoom);
         zoom -= res;
         setTimeout(function () {
             zoom += res;
@@ -104,18 +97,12 @@ export function initWASD(canvas) {
     }
 }
 
-export function getKeyPan(index) {
-    const res = mov[index] + moveOnce[index];
-    moveOnce[index] = 0.0;
-
-    return res;
-}
-
 export function getCamPan() {
-    camPan[1] -= getKeyPan(0) * (camPan[1] > -camPanLimitVert); 
-    camPan[1] += getKeyPan(2) * (camPan[1] <  camPanLimitVert); 
-    camPan[0] -= getKeyPan(3) * (camPan[0] > -camPanLimitHor); 
-    camPan[0] += getKeyPan(1) * (camPan[0] <  camPanLimitHor); 
+    camPan[0] += (mov[0] + moveOnce[0]) * zoomCoef;
+    moveOnce[0] = 0.0;
+
+    camPan[1] -= (mov[1] + moveOnce[1]) * zoomCoef;
+    moveOnce[1] = 0.0;
 
     return camPan;
 }
@@ -125,7 +112,7 @@ export function getZoom() {
     if (camZoom <= camZoomMax && camZoom >= camZoomMin) {
         camZoom += res;
     }
-    else if (camZoom > camZoomMax){
+    else if (camZoom > camZoomMax) {
         camZoom -= 0.004;
     }
     else {
