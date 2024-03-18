38ec0224d06d107401dc1d09c20c2927712b481c
Sean Bikkes <bixkitts@gmail.com>
2024-03-18 16:37:50 +0100

    character sheet work

--- test-clients/website/src/charsheet.js
+++ test-clients/website/src/charsheet.js
@@ -11,26 +11,86 @@ var playerStats = {
                   "Clown"],
         index: 0,
         element: document.getElementById('selectedPlayerBackground'),
-        input: document.getElementById('playerBackgroundInput')
+        input: document.getElementById('playerBackgroundInput'),
+        pointPoolModifier: 0
     },
     "gender": {
         options: ["Male", 
                   "Female"],
         index: 0,
         element: document.getElementById('selectedPlayerGender'),
-        input: document.getElementById('playerGenderInput')
+        input: document.getElementById('playerGenderInput'),
+        pointPoolModifier: 0
+    },
+};
+var playerSkills = {
+    "vigour": {
+        options: ["1", 
+                  "2",
+                  "3",
+                  "4",
+                  "5",
+                  "6",
+                  "7",
+                  "8",
+                  "9",
+                  "10"],
+        index: 0,
+        element: document.getElementById('selectedPlayerVigour'),
+        input: document.getElementById('playerVigourInput'),
+        pointPoolModifier: 1
+    },
+    "violence": {
+        options: ["1", 
+                  "2",
+                  "3",
+                  "4",
+                  "5",
+                  "6",
+                  "7",
+                  "8",
+                  "9",
+                  "10"],
+        index: 0,
+        element: document.getElementById('selectedPlayerViolence'),
+        input: document.getElementById('playerViolenceInput'),
+        pointPoolModifier: 1
+    },
+    "guile": {
+        options: ["1", 
+                  "2",
+                  "3",
+                  "4",
+                  "5",
+                  "6",
+                  "7",
+                  "8",
+                  "9",
+                  "10"],
+        index: 0,
+        element: document.getElementById('selectedPlayerGuile'),
+        input: document.getElementById('playerGuileInput'),
+        pointPoolModifier: 1
     },
     // Add more player statistics here as needed
 };
+var pointPool        = 10;
+var pointPoolElement = document.getElementById('pointPoolCounter');
 
-function updateSelectedOption(statistic) {
+function updateSelectedStatOption(statistic) {
     var stat                 = playerStats[statistic];
     stat.element.textContent = stat.options[stat.index];
     stat.input.value         = stat.options[stat.index];
 }
+function updateSelectedSkillOption(statistic) {
+    var stat                 = playerSkills[statistic];
+    stat.element.textContent = stat.options[stat.index];
+    stat.input.value         = stat.options[stat.index];
 
-// Update event listeners for left arrow and right arrow
-function updateEventListeners(statistic) {
+    pointPoolElement.textContent = pointPool;
+}
+
+function updateStatEventListeners(statistic) {
     var stat = playerStats[statistic];
     
     document.getElementById(statistic + 'LeftArrow').addEventListener('click', function() {
@@ -40,7 +100,7 @@ function updateEventListeners(statistic) {
         else {
             stat.index = stat.options.length - 1;
         }
-        updateSelectedOption(statistic);
+        updateSelectedStatOption(statistic);
     });
     
     document.getElementById(statistic + 'RightArrow').addEventListener('click', function() {
@@ -50,13 +110,40 @@ function updateEventListeners(statistic) {
         else {
             stat.index = 0;
         }
-        updateSelectedOption(statistic);
+        updateSelectedStatOption(statistic);
     });
 }
 
+function updateSkillEventListeners(statistic) {
+    var stat = playerSkills[statistic];
+    
+    document.getElementById(statistic + 'LeftArrow').addEventListener('click', function() {
+        if (stat.index > 0) {
+            stat.index -= stat.pointPoolModifier;
+            pointPool  += stat.pointPoolModifier;
+        }
+        updateSelectedSkillOption(statistic);
+    });
+    
+    document.getElementById(statistic + 'RightArrow').addEventListener('click', function() {
+        if (stat.index < stat.options.length - 1 && pointPool > 0) {
+            stat.index += stat.pointPoolModifier;
+            pointPool  -= stat.pointPoolModifier;
+        }
+        updateSelectedSkillOption(statistic);
+    });
+}
+
+// Initialization....
 for (var stat in playerStats) {
-    updateEventListeners(stat);
+    updateStatEventListeners(stat);
+}
+for (var skill in playerSkills) {
+    updateSkillEventListeners(skill);
 }
 for (var stat in playerStats) {
-    updateSelectedOption(stat);
+    updateSelectedStatOption(stat);
+}
+for (var skill in playerSkills) {
+    updateSelectedSkillOption(skill);
 }
