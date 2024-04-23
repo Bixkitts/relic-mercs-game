853c15338f65f4025bc024220e3a8c7b4cc6e394
MichalWojcik0 <michal_wojcik@zohomail.eu>
2024-04-23 18:22:38 +0200

    can i see text, you dum dum

--- test-clients/website/src/rendering/resource-loading.js
+++ test-clients/website/src/rendering/resource-loading.js
@@ -44,13 +44,8 @@ export function loadTexture(gl, url) {
     const image = new Image();
     image.onload = () => {
         gl.bindTexture (gl.TEXTURE_2D, texture);
-        gl.texImage2D  (gl.TEXTURE_2D,
-                        level,
-                        internalFormat,
-                        srcFormat,
-                        srcType,
-                        image,
-        );
+        gl.texImage2D  (gl.TEXTURE_2D, level, internalFormat, 
+                        srcFormat, srcType, image);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
         gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
